eae7fc2e44994429aac8d7ede7c9ff8c47ae7c1b
root <watson@example.com>
2020-03-14 22:12:23 +0800

    update version to v0.02 	modified:   main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define VERSION "0.01"
+#define VERSION "0.02"
 int main(int argc, char **argv)
 {
-	printf("The version is %s\n",VERSION);
+	printf("The version is %s@ add A design\n",VERSION);
 	return 0;
 }
