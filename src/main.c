532337983ed682e166ec48e853d34068dd56d19f
root <watson@example.com>
2020-03-15 15:17:19 +0800

    fix #2 about init to read ini for sqlite. 	Just check the issue #2 (Bug and enhencenment)
    	renamed:    file/tetst.ini -> file/test.ini
    	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #define VERSION "0.02"
+
+/*set line in sqlite*/
+
+static int token_for_sqlite(char *line)
+{
+	char *front = NULL;
+	char *last  = NULL;
+	
+	assert(line);
+
+	front = strtok(line,"=");
+
+	if (front != NULL)
+	{	
+		last = strtok(NULL,"=");
+		if (last != NULL)
+			printf("%s|%s\n",front,last);
+		else
+			return -1;
+	}
+	else
+		return -1;
+		
+	return 0;
+}
+
+/*read ini and set in sqlite*/
+static int read_ini_for_sqlite()
+{
+	FILE *file = NULL;
+	char line[128] = "";
+	int res = 0;	
+	
+	//open ini file as descriptor
+	file = fopen("../file/test.ini","r");
+	if (NULL == file)
+	{
+		perror("Error in fopen");
+		return -1;
+	}
+
+	//read each line of ini file
+	while(fgets(line,128,file) != NULL)
+	{
+		if (strstr(line,"=") != NULL)
+		{
+			res = token_for_sqlite(line);	
+			if (res == -1)
+				return -1;
+		}
+		else
+			continue;
+	}//end of while
+	return 0;
+}
+
+
 int main(int argc, char **argv)
 {
 	printf("The version is %s@ add A design\n",VERSION);
+	read_ini_for_sqlite();
 	return 0;
 }
